77d64f31c0ddfb3874fd2123338a540fac25eedf
lingbei <lingbei@wacai.com>
2019-04-04 12:25:23 +0800

    新增一个espresso test demo

--- app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/myapplication/ExampleInstrumentedTest.java
@@ -2,11 +2,18 @@ package com.example.myapplication;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.clearText;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -16,10 +23,20 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
-        assertEquals("com.example.myapplication", appContext.getPackageName());
+    public void MainActivityInputMessageTest() {
+        // Context of the app under test
+
+        onView(withId(R.id.editText3)).perform(clearText(),typeText("lingbei"));
+        onView(withId(R.id.button)).perform(click());
+
+        onView(withId(R.id.textView2)).toString().matches("lingbei");
+
+
     }
 }
